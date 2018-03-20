a588783bb8fc469a500c36c64b8036ba42e34477
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-20 20:16:28 +0000

    Add post(/engineers to EngineersController

--- src/main/java/controllers/EngineersController.java
+++ src/main/java/controllers/EngineersController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import db.DBHelper;
+import models.Department;
 import models.Engineer;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -29,6 +30,38 @@ public class EngineersController {
             model.put("engineers", engineers);
 
             return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+        get("/engineers/new", (req, res) -> {
+
+            List<Department> departments = DBHelper.getAll(Department.class);
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/engineers/create.vtl");
+            model.put("departments", departments);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+        post("/engineers", (req, res) -> {
+
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+
+            Department department = DBHelper.find(departmentId, Department.class);
+
+            String firstName = req.queryParams("firstName");
+            String lastName = req.queryParams("lastName");
+            int salary = Integer.parseInt(req.queryParams("salary"));
+
+            Engineer engineer = new Engineer(firstName, lastName, salary, department);
+
+            DBHelper.save(engineer);
+
+            res.redirect("/engineers");
+
+            return null;
 
         }, new VelocityTemplateEngine());
 
